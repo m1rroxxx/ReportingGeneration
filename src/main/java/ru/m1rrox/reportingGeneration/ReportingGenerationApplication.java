16a8447f12b410447c4f57a2ce2b91af2c725881
nikitasidorov <Nikita.sidorov.7@icloud.com>
2025-05-10 00:37:44 +0300

    readme

---
+++ src/main/java/ru/m1rrox/reportingGeneration/ReportingGenerationApplication.java
@@ -0,0 +1,13 @@
+package ru.m1rrox.reportingGeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportingGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportingGenerationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/m1rrox/reportingGeneration/ReportingGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.m1rrox.reportingGeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
